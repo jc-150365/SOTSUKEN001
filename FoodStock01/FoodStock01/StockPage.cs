315838388084c8ef2b861d434f58f46c65571b39
謙太 可士村 <150365@jc-21.jp>
2017-11-15 16:19:55 +0900

    あああ

--- FoodStock01/FoodStock01/StockPage.cs
+++ FoodStock01/FoodStock01/StockPage.cs
@@ -18,7 +18,7 @@ namespace FoodStock01
             var label1 = new Label
             {
                 FontSize = 40,
-                //ビューの中央に配置。
+                //ビューの中央に配置
                 HorizontalOptions = LayoutOptions.Center,
                 VerticalOptions = LayoutOptions.Start,
                 Text = title
